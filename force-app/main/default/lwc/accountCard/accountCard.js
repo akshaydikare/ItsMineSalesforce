83d5dbc9b0fa0ffc89236292ed27a3e62592a9a0
akshaydikare <akshaydikare@gmail.com>
2025-07-03 09:46:27 +0530

    lwc - account card example- Modified

--- force-app/main/default/lwc/accountCard/accountCard.js
+++ force-app/main/default/lwc/accountCard/accountCard.js
@@ -1,11 +1,12 @@
 import { LightningElement } from 'lwc';
-import '@salesforce/apex/AccountDAL.getAccount';
+import getAccount from '@salesforce/apex/AccountDAL.getAccount';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { NavigationMixin } from 'lightning/navigation';
 
 export default class AccountCard extends LightningElement {
 
     account;
+    isLoading = true;
 
     connectedCallback() {
         this.fetchAccount();
@@ -13,11 +14,27 @@ export default class AccountCard extends LightningElement {
 
     fetchAccount() {
         getAccount()
-            .then(result => {
+            .then(result => {              
+                if (!result) {
+                    this.showErrorToast({ body: { message: 'No account found' } });
+                    return;
+                }
+                else if( result.Name === undefined || result.Rating === undefined) {
+                    this.showErrorToast({ body: { message: 'Account data is incomplete'} });
+                    return;
+                }else(
+                    this.isLoading = false,
+                    console.log('Account fetched successfully:', result)
+                );
                 this.account = result;
+                console.log(this.account.Name);
+                console.log(this.account.Rating);
+              
             })
             .catch(error => {
+                   this.isLoading = false,
                 this.showErrorToast(error);
+                console.error('Error fetching account:', error);
             });
     }
 
